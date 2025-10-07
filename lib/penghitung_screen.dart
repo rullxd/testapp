72ee75c0c7d78e4d086afc9e0f5c90169a216d74
rullxd <122759599+rullxd@users.noreply.github.com>
2025-10-07 12:38:21 +0700

    Belajar Statefull

---
+++ lib/penghitung_screen.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class Penghitungscreen extends StatefulWidget {
+  const Penghitungscreen({super.key});
+
+  @override
+  State<Penghitungscreen> createState() => _PenghitungscreenState();
+}
+
+class _PenghitungscreenState extends State<Penghitungscreen> {
+  int nilai = 0;
+
+  menghitung() {
+    setState(() {});
+    nilai = nilai + 1;
+    print("ini nilai = $nilai");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blueGrey,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("Hitung $nilai"),
+            ElevatedButton(
+              onPressed: () {
+                menghitung();
+              },
+              child: Text("Hitung"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
